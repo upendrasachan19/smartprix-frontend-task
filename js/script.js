bca452816cbbbcb5c3497ab87bd01cdf1e44c04f
Upendra Sachan <upendrasinghsachan@gmail.com>
2018-05-04 19:52:01 +0530

    phone fight

--- js/script.js
+++ js/script.js
@@ -25,3 +25,15 @@ $(document).ready(function(){
         }
     );
   });
+    // Initialize a new plugin instance for all
+    // e.g. $('input[type="range"]') elements.
+    $('input[type="range"]').rangeslider();
+
+    // Destroy all plugin instances created from the
+    // e.g. $('input[type="range"]') elements.
+    $('input[type="range"]').rangeslider('destroy');
+
+    // Update all rangeslider instances for all
+    // e.g. $('input[type="range"]') elements.
+    // Usefull if you changed some attributes e.g. `min` or `max` etc.
+    $('input[type="range"]').rangeslider('update', true);
