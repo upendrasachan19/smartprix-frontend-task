289863ee6e3cede8c384f8a1111843910697c7f6
Upendra Sachan <upendrasinghsachan@gmail.com>
2018-05-05 07:05:05 +0530

    Dummy data- Final Commit

--- js/script.js
+++ js/script.js
@@ -7,33 +7,18 @@ $(document).ready(function(){
             autoplayTimeout: 3000,
             autoplayHoverPause:true,
             margin: 15,
-            // responsive:{
-            //     0:{
-            //         items:1,
-            //         nav:true
-            //     },
-            //     600:{
-            //         items:3,
-            //         nav:false
-            //     },
-            //     1000:{
-            //         items:5,
-            //         nav:true,
-            //         loop:false
-            //     }
-            // }
+            responsive:{
+                0:{
+                    items:1,
+                },
+                600:{
+                    items:4,
+                },
+                1000:{
+                    items:4,
+                }
+            }
         }
     );
   });
-    // Initialize a new plugin instance for all
-    // e.g. $('input[type="range"]') elements.
-    $('input[type="range"]').rangeslider();
 
-    // Destroy all plugin instances created from the
-    // e.g. $('input[type="range"]') elements.
-    $('input[type="range"]').rangeslider('destroy');
-
-    // Update all rangeslider instances for all
-    // e.g. $('input[type="range"]') elements.
-    // Usefull if you changed some attributes e.g. `min` or `max` etc.
-    $('input[type="range"]').rangeslider('update', true);
